aed6b7af8a6815dd008858b36e643ef6b0f018c9
dmitri <dmitri@postplane.com>
2021-06-04 14:43:36 -0700

    Initial Commit

---
+++ gifworld/gifworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  gifworldApp.swift
+//  gifworld
+//
+//  Created by dmitri lemmerman on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct gifworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
